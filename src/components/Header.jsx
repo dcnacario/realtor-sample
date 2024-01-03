0bb9efd75672262374ecc309c01c8984fb1cbf56
Danmar Cornelio Nacario <dcnacario08@gmail.com>
2024-01-03 20:15:34 +0800

    add: more functionalities for header making it dynamic when a user login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function Header() {
+  const [pageState, setPageState] = useState("Sign in");
   const location = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile");
+      } else {
+        setPageState("Sign in");
+      }
+    });
+  }, [auth]);
   function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true;
@@ -44,13 +56,13 @@ export default function Header() {
             </li>
             <li
               className={
-                pathMatchRoute("/sign-in")
+                pathMatchRoute("/sign-in") || pathMatchRoute("/profile")
                   ? `cursor-pointer py-3 text-sm font-semibold text-black border-b-[3px] border-b-red-500`
                   : `cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent`
               }
-              onClick={() => navigate("/sign-in")}
+              onClick={() => navigate("/profile")}
             >
-              Sign in
+              {pageState}
             </li>
           </ul>
         </div>
